97e97f3d1d6a8eb72f85cd4a4b18567a0d39b155
Kiran9901 <pskiran9901@gmail.com>
2021-08-11 16:56:15 +0530

    many changes

--- app.js
+++ app.js
@@ -8,3 +8,11 @@
 // console.log(require("fs")); //Just to know about teh contents present in the fs module, which stands for filesync module
 // const fs_sync = require("./fs-sync"); //readFileSync returns both the files content that is printed to the console
 // const fs_async = require("./fs-async"); //here, readFile doesn't return the result, so there's no way to acces the file contents onto the cglobal scope directly, so undefined is printed on the console
+
+//const server = require("./server");//Code to run a server instance present in the server.js file
+
+//npm - Also abbreviated as node package manager is one of the biggest code repo that contains reusable javascript code, in the form of npm modules. If one wants, they can share their code to the npm community or one can even retrieve code from the repo, locally or globally
+
+//npm --version ->To check the npm version being used in the machine
+//npm i <package_name> -> Installs the package mentioned in the package_name argument local to the current project
+//npm install -g <package_name> -> Installs the package mentioned in the package_name globally
