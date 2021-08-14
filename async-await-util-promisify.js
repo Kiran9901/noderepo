7d20cc2ee32a0c8ea25635cdeeba9cf0930c5449
Kiran9901 <pskiran9901@gmail.com>
2021-08-14 13:06:23 +0530

    changes

--- async-await-util-promisify.js
+++ async-await-util-promisify.js
@@ -1,9 +1,9 @@
-const { readFile, writeFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
 
 const util = require("util");
 
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 //Hard coded version of promise
 // const getText = (path) => {
@@ -16,15 +16,14 @@ const writeFilePromise = util.promisify(writeFile);
 // };
 
 const execute = async () => {
-  const first = await readFilePromise("./images/cricketers/first.txt", "utf8");
-  const second = await readFilePromise(
-    "./images/cricketers/second.txt",
-    "utf8"
-  );
-  await writeFilePromise(
+  //async makes the function asynchronous, thus avoiding blocking code
+  const first = await readFile("./images/cricketers/first.txt", "utf8");
+  //await makes sure that the result is returned properly after completely finishing the readfile operation. Same goes with the others too.
+  const second = await readFile("./images/cricketers/second.txt", "utf8");
+  await writeFile(
     "./images/cricketers/third_async_await",
-    `This is cool: ${first} merged with ${second}`
-  );
+    `This is cool: ${first} which is merged with ${second}`
+  ); //We can even have a flag as a third argument like this: {flag: 'a'}, to write to a file in append mode
   console.log(first, second);
 };
 
